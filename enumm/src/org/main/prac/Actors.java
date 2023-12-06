e6a8a286390932a317903a67822be22b21584ae9
kalaivananJava <146086997+kalaivananJava@users.noreply.github.com>
2023-12-06 10:28:52 +0530

    Update Actors.java

--- enumm/src/org/main/prac/Actors.java
+++ enumm/src/org/main/prac/Actors.java
@@ -1,25 +1,26 @@
 package org.main.prac;
 
 public enum Actors {
-
+//constants only first then other statements....
 	VIJAY(50){
 		public void act() {
-			System.out.println("dance.....");
+			System.out.println("dance.....");//this method is for specific actor
 		}
 
 
 	},AJITH(40),SIVA(30),POWERSTAR{
 
 		public void act() {
-			System.out.println("poweru....");
+			System.out.println("poweru....");//this method is for specific actor
 		}
 	}, VIJAYSETHUPATHI(20),SURYA(10){
 		public void act() {
-			System.out.println("suriya....");
+			System.out.println("suriya....");//this method is for specific actor
 		}
 	};
 	public void act() {
-		System.out.println("Normal....");
+		System.out.println("Normal....");//once we give the method specifically 
+		//we have to give default method for the other constants who does n't have specific method otherwise we cant access it.
 	}
 	Actors(){
 
